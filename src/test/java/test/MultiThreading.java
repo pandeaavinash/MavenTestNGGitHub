08bc846a0967cf32aa2fc483a4f316c732d9b661
Avinash Pande <aavinashpande@gmail.com>
2018-01-14 17:53:19 +0530

    <apande>adding pom,testNg & multithreading classes

--- src/test/java/test/MultiThreading.java
+++ src/test/java/test/MultiThreading.java
@@ -3,8 +3,16 @@ package test;
 import org.testng.annotations.Test;
 
 public class MultiThreading {
-  @Test
+  
+  @Test(groups={"Integration"},priority=0)
   public void Read() 
   {
+	  System.out.println("In Read method of Integration group");
+  }
+  
+  @Test(groups={"Regression"},dependsOnGroups="Integration")
+  public void write()
+  {
+	  System.out.println("In Write method of Regression group");
   }
 }
