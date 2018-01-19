b581d374eae43b1ef7bd94bcfeb24cea06e093eb
Avinash Pande <aavinashpande@gmail.com>
2018-01-19 22:28:21 +0530

    <apande>added new test

--- src/test/java/test/NewTest.java
+++ src/test/java/test/NewTest.java
@@ -2,10 +2,25 @@ package test;
 
 import org.testng.annotations.Test;
 
-public class NewTest {
+import com.relevantcodes.extentreports.LogStatus;
+
+import resources.CustomReport;
+import resources.Report;
+
+public class NewTest 
+{
   @Test
-  public void f() 
+  public void Test1() 
   {
 	  System.out.println("hi");
+	  Report.test.log(LogStatus.PASS, "pass");
+	  //Report.test.log(LogStatus.PASS, "avinash");
+  }
+  @Test
+  public void Test2() 
+  {
+	  System.out.println("hi");
+	  Report.test.log(LogStatus.PASS, "pass");
+	  //Report.test.log(LogStatus.PASS, "avinash");
   }
 }
