5a02da8b176e0366ef0aae5ee6eb638cdc3a6ff5
Avinash Pande <aavinashpande@gmail.com>
2018-02-03 10:55:35 +0530

    <apande> Added ExtentReport class

---
+++ src/test/java/test/ExtentReportTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import resources.Report;
+
+public class ExtentReportTest 
+{
+	@Test
+	  public void Test3() 
+	  {
+		  System.out.println("hi");
+		  Report.test.log(LogStatus.PASS, "pass");
+		  //Report.test.log(LogStatus.PASS, "avinash");
+	  }
+	  @Test
+	  public void Test4() 
+	  {
+		  System.out.println("hi");
+		  Report.test.log(LogStatus.PASS, "pass");
+		  //Report.test.log(LogStatus.PASS, "avinash");
+	  }
+
+}
